49e4955f62fb3587d618ef151ec9ac8be86672ac
IC7K <27rusk@gmail.com>
2013-07-24 23:21:09 +1100

    NO SPACE LEFT

--- dump1090.c
+++ dump1090.c
@@ -1824,7 +1824,7 @@ void interactiveShowData(void) {
 
     printf("\x1b[H\x1b[2J");    /* Clear the screen */
     printf(
-" Hex%s Flight   Alt    Spd  Trk  Tout  VSpd\n"
+"Hex%s Flight   Alt    Spd  Trk  Tout  VSpd\n"
 " -------------------------------------------\n",
         progress);
 /*
@@ -1886,19 +1886,19 @@ void interactiveShowData(void) {
 */		
 		if (flevel[0]=='\0') /* below 1000m all in meters, above in FlightLevels ex. FL330 */
 		{
-        printf(" %-6s %-6s %-6d %-4d  %-3d  %d %-4d\n",
+        printf("%-6s %-6s %-6d %-4d  %-3d  %d %-4d\n",
 		a->hexaddr, a->flight, altitude, speed, a->track,
 		(int)(now - a->seen), vspeed);	
 		} else
 			{
 			if (flevel[2]=='\0') /* if 'FL' then 2 symbols, if 'FL0' then 3 symbols string show */
 				{
-				printf(" %-6s %-6s %-2s%-3d  %-4d  %-3d  %d %-4d\n",
+				printf("%-6s %-6s %-2s%-3d  %-4d  %-3d  %d %-4d\n",
 				a->hexaddr, a->flight, altitude, speed, a->track,
 				(int)(now - a->seen), vspeed);		
 				} else
 				{
-				printf(" %-6s %-6s %-3s%-3d %-4d  %-3d  %d %-4d\n",
+				printf("%-6s %-6s %-3s%-3d %-4d  %-3d  %d %-4d\n",
 				a->hexaddr, a->flight, altitude, speed, a->track,
 				(int)(now - a->seen), vspeed);		
 				}
@@ -1912,7 +1912,7 @@ void interactiveShowData(void) {
         count++;
     }
 	
-	printf("\n adsbradar.ru 2013\n Aviation Technologies\n");		
+	printf("\nadsbradar.ru 2013\n Aviation Technologies\n");		
 }
 
 /* When in interactive mode If we don't receive new nessages within
