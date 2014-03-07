b2cdbcd345f8395cf77325f4bb29ce69001c6d9c
IC7K <27rusk@gmail.com>
2014-03-07 18:41:08 +1100

    -1SPACE

--- dump1090.c
+++ dump1090.c
@@ -1977,8 +1977,8 @@ void interactiveShowData(void) {
         a = a->next;
         count++;
     }
-    while(count < 10) {printf("                                                \n"); count++;}
-	printf("\x1b[0;0;0;37mADS-B Radar v.2     adsbradar.aero 2011-2014 \n");	// \n в конце не убирать! иначе мерцание!	
+    while(count < 10) {printf("                                               \n"); count++;}
+	printf("\x1b[0;0;0;37mADS-B Radar v.3     adsbradar.aero 2011-2014 \n");	// \n в конце не убирать! иначе мерцание!	
 }
 
 /* When in interactive mode If we don't receive new nessages within
