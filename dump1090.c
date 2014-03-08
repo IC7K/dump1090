e3087211f0c1a913f700f32381b969fc1389e259
IC7K <27rusk@gmail.com>
2014-03-08 16:55:12 +1100

    NULL

--- dump1090.c
+++ dump1090.c
@@ -1889,7 +1889,7 @@ void interactiveShowData(void) {
         progress);
 */		
 
-    progress[0] = '\0';
+    progress=NULL;
 
     // while(a && count < Modes.interactive_rows) {
     while(a && count < 10) {        //13 строк всего на экране
