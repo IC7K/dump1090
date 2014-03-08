561af4b830da41d5e012688f4888582a124487ca
IC7K <27rusk@gmail.com>
2014-03-09 10:07:59 +1100

    -progr

--- dump1090.c
+++ dump1090.c
@@ -1995,7 +1995,7 @@ void interactiveShowData(void) {
         count++;
     }
     while(count < 10) {printf("                                             \n"); count++;}
-	printf("\x1b[0;0;0;37mADS-B Radar v.3     adsbradar.aero 2011-2014 \n", progress);	// \n в конце не убирать! иначе мерцание!	
+	printf("\x1b[0;0;0;37mADS-B Radar v.3     adsbradar.aero 2011-2014 \n");	// \n в конце не убирать! иначе мерцание!	
     // free(progress);
 }
 
